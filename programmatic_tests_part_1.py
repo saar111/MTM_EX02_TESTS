31081a1a30c4addeba0ef96dd800f701d0a0af1d
Saar Ofek <10295509+saar111@users.noreply.github.com>
2020-12-26 01:03:45 +0200

    part 1 finished

--- programmatic_tests_part_1.py
+++ programmatic_tests_part_1.py
@@ -51,8 +51,8 @@ class Tests(object):
             "(Wrong value was: " + str(runYoungStudentsWithInputName(input_file_name, 1)) + ")"
 
     @staticmethod
-    def testYoungStudentsManyWrongStudentsAndGenerallyShit():
-        input_file_name = "3.1.2.1_young_students_many_wrong_students_and_generally_shit_k_100000.txt"
+    def testYoungStudentsManyWrongStudentsBigTest():
+        input_file_name = "3.1.2.1_young_students_many_wrong_students_and_big_dataset_100000.txt"
         assert runYoungStudentsWithInputName(input_file_name, 10000) == 20,\
             "(Wrong value was: " + str(runYoungStudentsWithInputName(input_file_name, 10000)) + ")"
         assert runYoungStudentsWithInputName(input_file_name, 17) == 17,\
