cd088cddf3fdd82e5972b7c09ae9f5bd21f3227e
Saar Ofek <10295509+saar111@users.noreply.github.com>
2020-12-29 16:20:26 +0200

    part 1 finished

--- programmatic_tests_part_2.py
+++ programmatic_tests_part_2.py
@@ -9,10 +9,6 @@ def assert_test(received_value, expected_value, error_message=None):
 
     assert received_value == expected_value, error_message
 
-def print_input_file_test_failure(input_name):
-    print(
-        "Expected: <a href='/ex02/staging/{STAGING_ID}/expected_outputs/" + input_name + "'>expected_" + input_name + "</a>, but got instead: <a href='/ex02/staging/{STAGING_ID}/outputs/" + input_name + "'>{}</a>".format(
-            input_name))
 
 class Tests(object):
     @staticmethod
@@ -27,9 +23,9 @@ class Tests(object):
         EM.destroyEventManager(em)
 
         input_name = "3.2.2_test_1.txt"
-        assert_test(filecmp.cmp("./outputs/3.2.2_test_1.txt", "./expected_outputs/3.2.2_test_1.txt"), True, "\nExpected: <a href='/ex02/staging/{STAGING_ID}/expected_outputs/" + input_name + "'>expected_" + input_name + "</a>, but got instead: <a href='/ex02/staging/{STAGING_ID}/outputs/" + input_name + "'>{}</a>".format(
-            input_name))
-
+        assert_test(filecmp.cmp("./outputs/3.2.2_test_1.txt", "./expected_outputs/3.2.2_test_1.txt"), True,
+                    "\nExpected: <a href='/ex02/staging/{STAGING_ID}/expected_outputs/" + input_name + "'>expected_" + input_name + "</a>, but got instead: <a href='/ex02/staging/{STAGING_ID}/outputs/" + input_name + "'>{}</a>".format(
+                        input_name))
 
     @staticmethod
     def testsPrintEventsListFunctionsWith2SameEarliestDates():
@@ -43,9 +39,9 @@ class Tests(object):
         EM.destroyEventManager(em)
 
         input_name = "3.2.2_test_2.txt"
-        assert_test(filecmp.cmp("./outputs/3.2.2_test_2.txt", "./expected_outputs/3.2.2_test_2.txt"), True, "\nExpected: <a href='/ex02/staging/{STAGING_ID}/expected_outputs/" + input_name + "'>expected_" + input_name + "</a>, but got instead: <a href='/ex02/staging/{STAGING_ID}/outputs/" + input_name + "'>{}</a>".format(
-            input_name))
-
+        assert_test(filecmp.cmp("./outputs/3.2.2_test_2.txt", "./expected_outputs/3.2.2_test_2.txt"), True,
+                    "\nExpected: <a href='/ex02/staging/{STAGING_ID}/expected_outputs/" + input_name + "'>expected_" + input_name + "</a>, but got instead: <a href='/ex02/staging/{STAGING_ID}/outputs/" + input_name + "'>{}</a>".format(
+                        input_name))
 
 
 TESTS = method_list = [getattr(Tests, func) for func in dir(Tests) if
