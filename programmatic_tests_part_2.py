6d7a553b3d7d281516931ee1565548497583caf5
Saar Ofek <10295509+saar111@users.noreply.github.com>
2020-12-29 15:46:09 +0200

    part 1 finished

--- programmatic_tests_part_2.py
+++ programmatic_tests_part_2.py
@@ -1,5 +1,6 @@
 import eventManager
 import event_manager as EM
+import filecmp
 
 
 def assert_test(received_value, expected_value, error_message=None):
@@ -16,14 +17,26 @@ class Tests(object):
                         {"name": "annual Rock & Metal party", "id": 2, "date": EM.dateCreate(21, 4, 2021)}, \
                         {"name": "Improv", "id": 3, "date": EM.dateCreate(13, 3, 2021)}, \
                         {"name": "Student Festival", "id": 4, "date": EM.dateCreate(13, 5, 2021)}, ]
-        em = eventManager.printEventsList(events_lists, "FILL***")
+        em = eventManager.printEventsList(events_lists, "./outputs/3.2.2_test_1.txt")
         for event in events_lists:
             EM.dateDestroy(event["date"])
         EM.destroyEventManager(em)
 
+        assert_test(filecmp.cmp("./outputs/3.2.2_test_1.txt", "./expected_outputs/3.2.2_test_1.txt"), True)
+
+
     @staticmethod
     def testsPrintEventsListFunctionsWith2SameEarliestDates():
-        pass
+        events_lists = [{"name": "New Year's Eve", "id": 1, "date": EM.dateCreate(30, 12, 2020)}, \
+                        {"name": "annual Rock & Metal party", "id": 2, "date": EM.dateCreate(21, 4, 2021)}, \
+                        {"name": "Improv", "id": 3, "date": EM.dateCreate(13, 3, 2009)}, \
+                        {"name": "Student Festival", "id": 4, "date": EM.dateCreate(13, 3, 2009)}]
+        em = eventManager.printEventsList(events_lists, "./outputs/3.2.2_test_2.txt")
+        for event in events_lists:
+            EM.dateDestroy(event["date"])
+        EM.destroyEventManager(em)
+
+        assert_test(filecmp.cmp("./outputs/3.2.2_test_1.txt", "./expected_outputs/3.2.2_test_1.txt"), True)
 
 
 
